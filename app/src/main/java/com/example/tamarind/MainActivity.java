53dba7150c8176ad3318b523ea3bbebc435ac6d9
mmchinmay555 <mmchinmay555@gmail.com>
2020-07-14 14:24:57 +0530

    layout

--- app/src/main/java/com/example/tamarind/MainActivity.java
+++ app/src/main/java/com/example/tamarind/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.tamarind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior bottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View bottomSheet = findViewById(R.id.bottomSheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
+
     }
 }
